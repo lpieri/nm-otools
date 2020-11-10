bf91d4d99d36ad7044fec5f522b1fe3a60cdec27
Louise P <cpieri@student.42.fr>
2020-11-10 14:58:53 +0100

    wip

--- srcs/nm/nm.c
+++ srcs/nm/nm.c
@@ -12,19 +12,38 @@
 
 #include "nm-otool.h"
 
-void	print_symbol(s_file file)
+void 	parse_segment(struct segment_command_64* seg)
+{
+	struct section_64		*section;
+	uint32_t				nscets;
+
+	section = (struct section_64*)((void*)seg + sizeof(struct
+		segment_command_64));
+	nscets = seg->nsects;
+	while (nscets--)
+	{
+		if (ft_strncmp(section->segname, "__text", 16)){
+			printf("%s\n", ft_hex64_to_char(swap_uint64t(section->addr)));
+			printf("texte__ = %s\n", seg->segname);
+		}
+		section = (void*)section + sizeof(struct section_64);
+	}
+//	printf("%s\n", seg->segname);
+}
+
+void	parse_macho(s_file file)
 {
 	uint32_t				ncmds;
 	struct load_command*	lc;
-//	struct segment_command*	cmd;
 
 	ncmds = ((struct mach_header_64*)file.ptr)->ncmds;
 	lc = (struct load_command*)(file.ptr + sizeof(struct mach_header_64));
-	printf("lc = %p, file.ptr = %p\n", lc, file.ptr);
 	while (ncmds--)
 	{
-		printf("lc = %p, lc->cmd = %x\n", lc, lc->cmd);
-		if (lc->cmd == LC_SEGMENT_64 || lc->cmd == LC_SEGMENT) {
+		if (lc->cmd == LC_SEGMENT_64 || lc->cmd == LC_SEGMENT)
+			parse_segment((struct segment_command_64*)lc);
+		else if (lc->cmd == LC_SYMTAB) {
+			// nm func
 			printf("lol\n");
 		}
 		lc = (void*)lc + lc->cmdsize;
@@ -43,6 +62,6 @@ int		ft_nm(char *filename)
 	}
 	if (check_macho_file(file) != 0)
 		return (FAILURE);
-	print_symbol(file);
+	parse_macho(file);
 	return (0);
 }
