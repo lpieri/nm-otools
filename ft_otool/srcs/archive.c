3cf75659a656d373f80851b792b9713304b50eda
Louise P <cpieri@student.42.fr>
2020-11-24 11:20:12 +0100

    otool finish

--- ft_otool/srcs/archive.c
+++ ft_otool/srcs/archive.c
@@ -19,28 +19,35 @@ static void 	print_name(char *name)
 	write(1, "\n", 1);
 }
 
+static void 	print_filename(char *name, char *file)
+{
+	write(1, name, ft_strlen(name));
+	write(1, "(", 1);
+	write(1, file, ft_strlen(file));
+	write(1, ")", 1);
+}
+
 int	parse_archive(t_file file)
 {
-	t_ar_header		*header;
+	t_ar_header		*h;
 	char			*str;
 	size_t			len;
-	t_file			new;
 
 	print_name(file.name);
-	file.ptr += SARMAG;
+	h = (t_ar_header*)(file.ptr + SARMAG);
+	file.ptr += ft_atol(h->ar_size) + sizeof(t_ar_header) + SARMAG;
 	while (file.ptr)
 	{
-		header = (t_ar_header*)file.ptr;
-		if (ft_atoi(header->ar_size) <= 0)
+		h = (t_ar_header*)file.ptr;
+		if (ft_atoi(h->ar_size) <= 0)
 			return (0);
+		print_filename(file.name, h->ar_fmag + 2);
 		str = file.ptr + sizeof(t_ar_header);
 		len = ft_strlen(str);
 		while (!str[len++])
 			;
-		new = (t_file){(void*)(str + len - 1), header->ar_fmag + 2,
-			ft_atol(header->ar_size)};
-		ft_otool(new);
-		file.ptr += ft_atol(header->ar_size) + sizeof(t_ar_header);
+		ft_otool((t_file){(void*)(str + len - 1), NULL, ft_atol(h->ar_size)});
+		file.ptr += ft_atol(h->ar_size) + sizeof(t_ar_header);
 	}
 	return (0);
 }
